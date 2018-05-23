bea8b5d85cb26e07de50c445626075e3ce21707c
rchappa1 <you@example.com>
2018-05-23 13:36:34 -0400

    parameterized

--- src/main/java/com/mymq/sample/configuration/C360ConnectionConfiguration.java
+++ src/main/java/com/mymq/sample/configuration/C360ConnectionConfiguration.java
@@ -3,6 +3,7 @@ package com.mymq.sample.configuration;
 import javax.jms.ConnectionFactory;
 
 import org.apache.camel.component.jms.JmsComponent;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
@@ -17,18 +18,32 @@ import com.ibm.msg.client.wmq.WMQConstants;
 @Configuration
 public class C360ConnectionConfiguration {
 	
+	@Value("${messaging.mq.host}")
+	private String host;
+	@Value("${messaging.mq.port}")
+	private int port;
+	@Value("${messaging.mq.channel}")
+	private String channel;
+	@Value("${messaging.mq.ccsid}")
+	private int ccsid;
+	@Value("${messaging.mq.queueManager}")
+	private String queueManager;
+	@Value("${messaging.mq.messaging.user}")
+	private String username;
+	@Value("${messaging.mq.messaging.password}")
+	private String password;
 	
 	
 	@Bean
 	public MQQueueConnectionFactory mqQueueConnectionFactory() {
 	    MQQueueConnectionFactory mqQueueConnectionFactory = new MQQueueConnectionFactory();
-	    mqQueueConnectionFactory.setHostName("localhost");
+	    mqQueueConnectionFactory.setHostName(host);
 	    try {
 	        mqQueueConnectionFactory.setTransportType(WMQConstants.WMQ_CM_CLIENT);
-	        mqQueueConnectionFactory.setCCSID(1208); //this should be parameterized, we should get this number from MQ admin
-	        mqQueueConnectionFactory.setChannel("DEV.ADMIN.SVRCONN");
-	        mqQueueConnectionFactory.setPort(1414);
-	        mqQueueConnectionFactory.setQueueManager("QM1");
+	        mqQueueConnectionFactory.setCCSID(ccsid); //this should be parameterized, we should get this number from MQ admin
+	        mqQueueConnectionFactory.setChannel(channel);
+	        mqQueueConnectionFactory.setPort(port);
+	        mqQueueConnectionFactory.setQueueManager(queueManager);
 	    } catch (Exception e) {
 	        e.printStackTrace();
 	    }
@@ -39,8 +54,8 @@ public class C360ConnectionConfiguration {
 	@Bean
 	public UserCredentialsConnectionFactoryAdapter userCredentialsConnectionFactoryAdapter(MQQueueConnectionFactory mqQueueConnectionFactory) {
 	    UserCredentialsConnectionFactoryAdapter userCredentialsConnectionFactoryAdapter = new UserCredentialsConnectionFactoryAdapter();
-	    userCredentialsConnectionFactoryAdapter.setUsername("admin");
-	    userCredentialsConnectionFactoryAdapter.setPassword("passw0rd");
+	    userCredentialsConnectionFactoryAdapter.setUsername(username);
+	    userCredentialsConnectionFactoryAdapter.setPassword(password);
 	    userCredentialsConnectionFactoryAdapter.setTargetConnectionFactory(mqQueueConnectionFactory);
 	    return userCredentialsConnectionFactoryAdapter;
 	}
